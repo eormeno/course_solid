3b983ee0bd8555aab646f0f4e5dfe6b93c467e37
Emilio Ormeño <eormeno@gmail.com>
2022-06-04 11:24:56 -0300

    update

--- src/clean-code/07-tarea.ts
+++ src/clean-code/07-tarea.ts
@@ -1,42 +1,74 @@
-(()=>{
+(() => {
 
     //* Aplicar el principio de responsabilidad única
     //* Priorizar la composición frente a la herencia
 
-    type HtmlType = 'input'|'select'|'textarea'|'radio';
-   
+    type HtmlType = 'input' | 'select' | 'textarea' | 'radio';
+
+    interface HtmlElementProps {
+        id: string
+        type: HtmlType
+    }
+
     class HtmlElement {
-        constructor(
-            public id: string,
-            public type: HtmlType,
-        ) {}
-    }
+        public id: string
+        public type: HtmlType
 
-    class InputAttributes extends HtmlElement {
-        constructor(
-            public value: string,
-            public placeholder: string,
-            id: string,
-        ) {
-            super(id, 'input');
+        constructor({ id, type }: HtmlElementProps) {
+            this.id = id
+            this.type = type
         }
     }
 
-    class InputEvents extends InputAttributes {
-        constructor( value: string, placeholder: string, id: string ) {
-            super( value, placeholder, id );
+    interface ItemAttributesProps {
+        value: string
+        placeholder: string
+    }
+
+    class InputAttributes {
+        public value: string
+        public placeholder: string
+
+        constructor({ value, placeholder }: ItemAttributesProps) {
+            this.value = value
+            this.placeholder = placeholder
+        }
+    }
+
+    interface InputElementProps {
+        id: string
+        value: string
+        placeholder: string
+    }
+
+    class InputEvents {
+
+        constructor() {
         }
 
-        setFocus() {};
-        getValue() {};
-        isActive() {};
-        removeValue() {};
+        setFocus() { };
+        getValue() { };
+        isActive() { };
+        removeValue() { };
     }
 
 
     //? Idea para la nueva clase InputElement
 
-    const nameField = new InputEvents('Fernando', 'Enter first name', 'txtName');
+    class InputElement {
+        public htmlElement: HtmlElement
+        public itemAttributes: InputAttributes
+        public inputEvents: InputEvents
+
+        constructor(value: string, placeholder: string, id: string) {
+            let type : HtmlType = 'input'
+            this.htmlElement = new HtmlElement({ id, type})
+            this.itemAttributes = new InputAttributes({ value, placeholder })
+            this.inputEvents = new InputEvents()
+        }
+    }
+
+    const nameField = new InputElement('Fernando', 'Enter first name', 'txtName');
 
     console.log({ nameField });
 
